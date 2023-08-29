b42320fcff4cb4f8db6d04f7f8a0d98acc8f47c7
Zeruya <sarachuzeruya@gmail.com>
2023-08-29 15:26:41 -0300

    Sitio web creado

---
+++ eventos.js
@@ -0,0 +1,8 @@
+documento.addEventListener("DOMContentLoaded", function() {
+let divElement = document.getElementById("alerta");
+
+divElement.addEventListener("click", function() {
+    alert("Hola! Soy el div");
+})
+});
+
